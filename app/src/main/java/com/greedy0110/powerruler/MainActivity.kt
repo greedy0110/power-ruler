dd8235fea1873538b2e9406c4841002fcf1058fa
greedy0110 <greedy0110@gmail.com>
2020-07-23 07:20:14 +0900

    INIT

---
+++ app/src/main/java/com/greedy0110/powerruler/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.greedy0110.powerruler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
