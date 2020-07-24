f354da8028b7e580fbe9be17a34a01cc8f96cc6b
greedy0110 <greedy0110@gmail.com>
2020-07-24 23:37:11 +0900

    ADD workout adapter in OneRepMaxActivity

--- app/src/main/java/com/greedy0110/powerruler/feature/onerep/OneRepMaxActivity.kt
+++ app/src/main/java/com/greedy0110/powerruler/feature/onerep/OneRepMaxActivity.kt
@@ -1,9 +1,16 @@
 package com.greedy0110.powerruler.feature.onerep
 
 import android.os.Bundle
+import android.view.ViewGroup
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.observe
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.greedy0110.powerruler.databinding.ActivityOneRepMaxBinding
+import com.greedy0110.powerruler.databinding.ItemOnerepWorkoutBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -11,6 +18,7 @@ class OneRepMaxActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityOneRepMaxBinding.inflate(layoutInflater) }
     private val viewModel: OneRepMaxViewModel by viewModels()
+    private val adapter by lazy { Adapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +26,57 @@ class OneRepMaxActivity : AppCompatActivity() {
 
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
+
+        initList()
     }
 
     private fun initList() {
+        binding.listWorkout.adapter = adapter
+        binding.listWorkout.layoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
 
+        viewModel.items.observe(this) {
+            adapter.submitList(it)
+        }
     }
+
+    //region RecyclerView.
+
+    private inner class Adapter : ListAdapter<OneRepMaxViewModel.ItemHolder, ViewHolder>(
+        object : DiffUtil.ItemCallback<OneRepMaxViewModel.ItemHolder>() {
+            override fun areItemsTheSame(
+                oldItem: OneRepMaxViewModel.ItemHolder,
+                newItem: OneRepMaxViewModel.ItemHolder
+            ): Boolean = oldItem == newItem
+
+            override fun areContentsTheSame(
+                oldItem: OneRepMaxViewModel.ItemHolder,
+                newItem: OneRepMaxViewModel.ItemHolder
+            ): Boolean = oldItem == newItem
+        }
+    ) {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            return ViewHolder(ItemOnerepWorkoutBinding.inflate(layoutInflater, parent, false))
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.onBind(getItem(position))
+        }
+    }
+
+    private inner class ViewHolder(
+        private val binding: ItemOnerepWorkoutBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun onBind(item: OneRepMaxViewModel.ItemHolder) {
+            binding.viewModel = viewModel
+            binding.workout = item.workout
+        }
+    }
+
+    //endregion
 }
--- app/src/main/java/com/greedy0110/powerruler/feature/onerep/OneRepMaxViewModel.kt
+++ app/src/main/java/com/greedy0110/powerruler/feature/onerep/OneRepMaxViewModel.kt
@@ -1,13 +1,18 @@
 package com.greedy0110.powerruler.feature.onerep
 
+import android.content.Context
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
+import com.greedy0110.powerruler.R
+import com.greedy0110.powerruler.feature.onerep.OneRepFormulaUseCase.Workout
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.runBlocking
 
 class OneRepMaxViewModel @ViewModelInject constructor(
+    @ApplicationContext private val context: Context,
     private val useCase: OneRepFormulaUseCase
 ) : ViewModel() {
 
@@ -16,14 +21,14 @@ class OneRepMaxViewModel @ViewModelInject constructor(
         runBlocking {
             useCase.setGoal(500.0)
 
-            useCase.setWeight(OneRepFormulaUseCase.Workout.DEAD_LIFT, 140.0)
-            useCase.setRepeat(OneRepFormulaUseCase.Workout.DEAD_LIFT, 2)
+            useCase.setWeight(Workout.DEAD_LIFT, 140.0)
+            useCase.setRepeat(Workout.DEAD_LIFT, 2)
 
-            useCase.setWeight(OneRepFormulaUseCase.Workout.SQUAT, 100.0)
-            useCase.setRepeat(OneRepFormulaUseCase.Workout.SQUAT, 10)
+            useCase.setWeight(Workout.SQUAT, 100.0)
+            useCase.setRepeat(Workout.SQUAT, 10)
 
-            useCase.setWeight(OneRepFormulaUseCase.Workout.BENCH_PRESS, 80.0)
-            useCase.setRepeat(OneRepFormulaUseCase.Workout.BENCH_PRESS, 10)
+            useCase.setWeight(Workout.BENCH_PRESS, 80.0)
+            useCase.setRepeat(Workout.BENCH_PRESS, 10)
         }
     }
 
@@ -36,4 +41,47 @@ class OneRepMaxViewModel @ViewModelInject constructor(
     val totalOneRep: LiveData<String> = liveData {
         emit("${useCase.getOneRepMax().toInt()}")
     }
+
+    val items: LiveData<List<ItemHolder>> = MutableLiveData(
+        Workout.values().map { ItemHolder(it) }
+    )
+
+    private val cachedOneRepBy = mutableMapOf<Workout, LiveData<String>>()
+    fun getOneRepBy(workout: Workout): LiveData<String> {
+        return cachedOneRepBy[workout] ?: kotlin.run {
+            val new = MutableLiveData(
+                "${useCase.getOneRepMaxBy(workout).toInt()}"
+            )
+            cachedOneRepBy[workout] = new
+            new
+        }
+    }
+
+    private val cachedWorkoutDetail = mutableMapOf<Workout, LiveData<String>>()
+    fun getWorkoutDetail(workout: Workout): LiveData<String> {
+        return cachedWorkoutDetail[workout] ?: kotlin.run {
+            val new = MutableLiveData(
+                "${useCase.getWeight(workout)?.toInt()}${unit.value} ${useCase.getRepeat(workout)}rep"
+            )
+            cachedWorkoutDetail[workout] = new
+            new
+        }
+    }
+
+    private val cachedWorkoutName = mutableMapOf<Workout, LiveData<String>>()
+    fun getWorkoutName(workout: Workout): LiveData<String> {
+        return cachedWorkoutName[workout] ?: kotlin.run {
+            val new = MutableLiveData(
+                when (workout) {
+                    Workout.DEAD_LIFT -> context.getString(R.string.dead_lift)
+                    Workout.SQUAT -> context.getString(R.string.squat)
+                    Workout.BENCH_PRESS -> context.getString(R.string.bench_press)
+                }
+            )
+            cachedWorkoutName[workout] = new
+            new
+        }
+    }
+
+    data class ItemHolder(val workout: Workout)
 }
