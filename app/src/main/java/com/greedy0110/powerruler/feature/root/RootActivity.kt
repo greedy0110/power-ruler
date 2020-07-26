6aa4a6f26c7606381db9ef6c03f93e6a8d1fc8db
greedy0110 <greedy0110@gmail.com>
2020-07-26 20:15:51 +0900

    ADD root activity for branch activity

---
+++ app/src/main/java/com/greedy0110/powerruler/feature/root/RootActivity.kt
@@ -0,0 +1,51 @@
+package com.greedy0110.powerruler.feature.root
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.postDelayed
+import com.greedy0110.powerruler.databinding.ActivityRootBinding
+import com.greedy0110.powerruler.feature.init.InitActivity
+import com.greedy0110.powerruler.feature.init.OneRepFragment
+import com.greedy0110.powerruler.usecase.UserSettingUseCase
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class RootActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityRootBinding.inflate(layoutInflater) }
+
+    @Inject
+    lateinit var userSettingUseCase: UserSettingUseCase
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        Handler().postDelayed(1000L) {
+            val goal = userSettingUseCase.getGoal()
+
+            if (goal == null) {
+                startActivity(
+                    Intent(
+                        this,
+                        InitActivity::class.java
+                    )
+                        .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                        .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                )
+            } else {
+                startActivity(
+                    Intent(
+                        this,
+                        OneRepFragment::class.java
+                    )
+                        .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                        .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                )
+            }
+        }
+    }
+}
